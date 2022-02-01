3c66e7b410bbf16e815bf61b9d3ccb137db249b9
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-02-01 10:24:58 -0800

    commit tree 8afc8978fcebb78e999006a05205160f5fd4e1b2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
 class AuthorsController < ApplicationController
-  
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     author = Author.find(params[:id])
 
@@ -7,9 +10,11 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+
   end
 
   private
@@ -17,5 +22,15 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  private
+
+  def render_unprocessable_entity_response(invalid)
+  render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  # def render_not_found_response
+  #   render json: { error: "Name can't be blank" }, status: :not_found
+  # end
   
 end
